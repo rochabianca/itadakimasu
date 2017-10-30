8c40fc8e4f535c0a663205a737682e1a332c78d4
GOCASE <gocase@Mac-mini-de-GOCASE.local>
2017-10-30 09:32:44 -0300

    inicio do site, preparações básicas

---
+++ web/gruntfile.js
@@ -0,0 +1,69 @@
+
+module.exports = function(grunt) {
+  // var mozjpeg = require('imagemin-mozjpeg');
+      // 1. All configuration goes here 
+      grunt.initConfig({
+          pkg: grunt.file.readJSON('package.json'),
+          
+          imagemin: {
+            options: {
+              optimizationLevel: 7,
+              //  use: [ mozjpeg() ]
+            },
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'img-src/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'img/'
+                }]
+            }
+        },
+        // inlinecss: {
+        //   main: {
+        //     options: {
+        //        webResources: {
+        //          images: false,
+        //        },
+        //     },
+        //     files: {
+        //       'template-inline.html': 'template.html'
+        //     }
+        //   }
+        // },
+
+        watch: {
+          livereload: {
+            options: {
+              livereload: true
+            },
+            files: [
+              'index.html',
+              'css/style.min.css',
+              'img/*.{png,jpg,gif}'
+            ],
+          },
+          images: {
+            files: ['img-src/*.{png,jpg,gif}' ],
+            tasks: ['imagemin'],
+            options: {
+            spawn: false,
+            }
+          },
+          // css: {
+          //   files: ['css/*.css'],
+          //   tasks: ['inlinecss']
+          // },
+          options: {
+            spawn: false,
+            }
+      }
+  
+      });
+      grunt.loadNpmTasks('grunt-contrib-watch');
+      grunt.loadNpmTasks('grunt-contrib-imagemin');
+      // grunt.loadNpmTasks('grunt-inline-css');
+
+      grunt.registerTask('default', ['watch']);
+  
+  };
