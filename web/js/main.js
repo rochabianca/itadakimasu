ba0da8dab2e95a0003d86259f3f87fa1fc93c680
Bianca <bianca.rocha1711@gmail.com>
2017-11-01 16:58:36 -0300

    implementado carrousel inicial

---
+++ web/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:false,
+            loop:false
+        }
+    }
+  })
+});
+
